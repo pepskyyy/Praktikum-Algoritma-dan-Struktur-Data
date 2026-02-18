97a83d17bb3e5a1ce7872694b64383669a887709
pepksyyy <ahamadrifqi13@gmail.com>
2026-02-18 17:34:26 +0700

    Percobaan 2

---
+++ Pertemuan-2/MahasiswaMain01.java
@@ -0,0 +1,15 @@
+public class MahasiswaMain01 {
+    public static void main(String[] args) {
+    
+    Mahasiswa01 mhs1 = new Mahasiswa01();
+    mhs1.nama = "Muhammad Ali Farhan";
+    mhs1.nim = "2241720171";
+    mhs1.kelas = "SI 2J";
+    mhs1.ipk = 3.55;
+
+    mhs1.tampilkanInfo();
+    mhs1.ubahKelas("SI 2K");
+    mhs1.updateIPK(3.60);
+    mhs1.tampilkanInfo();
+    }
+}
