86a64d74fd43e1cf6dff44ee6866fca1c5db7bf3
pepskyyy <peduutrifqi@gmail.com>
2026-05-18 23:15:54 +0700

    buat class model Pesanan01 untuk menampung data menu restoran

---
+++ Case Method 2/Pesanan01.java
@@ -0,0 +1,13 @@
+public class Pesanan01 {
+    String namaPesanan;
+    int kodePesanan;
+    int harga;
+
+    Pesanan01(String namaPesanan, int kodePesanan, int harga) {
+        this.namaPesanan = namaPesanan;
+        this.kodePesanan = kodePesanan;
+        this.harga = harga;
+    }
+
+    
+}
