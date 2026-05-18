94b05390d6fd3766ab6f90d4fa9d7e05b0dc2667
pepskyyy <peduutrifqi@gmail.com>
2026-05-18 23:09:44 +0700

    class untuk menyimpan atribut pembeli

---
+++ Case Method 2/Pembeli01.java
@@ -0,0 +1,11 @@
+public class Pembeli01 {
+    String namaPembeli;
+    String NoHp;
+    int noAntrean;
+
+    Pembeli01(String namaPembeli, String NoHp, int noAntrean) {
+        this.namaPembeli = namaPembeli;
+        this.NoHp = NoHp;
+        this.noAntrean = noAntrean;
+    }
+}
